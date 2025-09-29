b8ad4832cf2c6b7117cb1048ae35e3048d2ba8ce
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2025-09-29 17:13:43 +0700

    feat: add tabs

---
+++ zerowaste-fe/app/(tabs)/_layout.tsx
@@ -0,0 +1,69 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { HapticTab } from '@/components/haptic-tab';
+import { Colors } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="analytics"
+        options={{
+          title: "Analytics",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="bar-chart-outline" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="leaderboard"
+        options={{
+          title: "Leaderboard",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="medal" size={26} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="feedback"
+        options={{
+          title: "Feedback",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="feedback" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="person-outline" size={28} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ zerowaste-fe/app/(tabs)/leaderboard.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text } from 'react-native';
+
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={<MaterialIcons name="home" size={28} color="#808080" />}
+    >
+      <Text>Leaderboard</Text>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
