d83a701daa3ecc365d30321c9252d8d0539234de
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2025-10-01 10:07:23 +0700

    feat: show header

--- zerowaste-fe/app/(tabs)/feedback.tsx
+++ zerowaste-fe/app/(tabs)/feedback.tsx
@@ -1,16 +1,10 @@
-import { StyleSheet, Text } from 'react-native';
-
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 
 export default function TabTwoScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={<MaterialIcons name="home" size={28} color="#808080" />}
-    >
+    <ScrollView>
       <Text>Feedback</Text>
-    </ParallaxScrollView>
+    </ScrollView>
   );
 }
 
